49813465ad859e932de49ad8e642bb6d2aabe341
Marcel Zak <marcel.zak13@gmail.com>
2016-10-04 18:59:54 +0100

    practical 2 is done

--- home/marcel/Documents/Robotics-CS3027/catkin_ws/build/auto_turtle/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/marcel/Documents/Robotics-CS3027/catkin_ws/src/auto_turtle
-# Build directory: /home/marcel/Documents/Robotics-CS3027/catkin_ws/build/auto_turtle
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- home/marcel/Documents/Robotics-CS3027/catkin_ws/build/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/gtest
-# Build directory: /home/marcel/Documents/Robotics-CS3027/catkin_ws/build/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
