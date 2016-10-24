13a8e685540fa5ff159cc8e70f4c8b12d99d4d11
Marcel Zak <marcel.zak13@gmail.com>
2016-10-25 00:22:50 +0100

    prioritization of goal points is done

--- catkin_ws/src/robot01/scripts/A_star.py
+++ catkin_ws/src/robot01/scripts/A_star.py
@@ -7,6 +7,7 @@
 """
 import math
 import rospy
+from sys import maxint
 from Queue import PriorityQueue
 from nav_msgs.msg import OccupancyGrid
 
@@ -26,37 +27,49 @@ class Node:
 		
 class Astar:
 	"""docstring for Astar"""
-	def __init__(self, grid = OccupancyGrid(), start_point_xy, goal_points_xy = [[0,0],[0,0]]):
+	def __init__(self,start_point_xy, goal_points_xy, grid = OccupancyGrid()):
 		self.grid = grid
 		self.start_point_xy = start_point_xy
-		if not self.goal_points_xy != [[0,0][0,0]]:
-			self.prioritized_goal_nodes = prioritize_goals(goal_points_xy)
+		if goal_points_xy:
+			pass
 		else:
 			print "No goals set!"
 			return 1
 		self.visitedQueue = []
 		self.priorityQueue = PriorityQueue()
 		self.solvedPath = []
-		self.priorityQueueGoals = PriorityQueue()
+		self.prioritizedGoals= []
+		goalNodes = PriorityQueue()
+		for i in range(len(goal_points_xy)): 
+			goalNodes.put(Node(goal_points_xy[i], priority=0))
+		self.__prioritize_nodes_heur(Node(self.start_point_xy, priority=0), goalNodes)
 		
-	def prioritize_goals(goals):
-		startNode = Node(self.start_point_xy)
-		goalNodes = []
-		unvisitedNodes = PriorityQueue()
-		for i in range(len(goals)): 
-			goalNodes.append(Node(goals[i]))
-		for i in range(len(goalNodes)):
-			goalNodes[i].priority = self.h(nodes[i],startNode)
-			unvisitedNodes.put(goalNodes[i])
-		self.priorityQueueGoals.put(unvisitedNodes.get())
-		for i in range(len(unvisitedNodes)):
-			node = 
+	def __prioritize_nodes_heur(self, start_node, goal_nodes = PriorityQueue()):
+		leftNodes = PriorityQueue()
+		if goal_nodes.qsize() > 1:
+			for i in range(goal_nodes.qsize()):
+				node = goal_nodes.get() 
+				node.priority = self.h(node,start_node)
+				leftNodes.put(node)
+			closestNode = leftNodes.get()
+			self.prioritizedGoals.append(closestNode.position_xy)
+			self.__prioritize_nodes_heur(closestNode,leftNodes)
+		else:
+			lastNode = goal_nodes.get()
+			lastNode.priority = maxint
+			self.prioritizedGoals.append(lastNode.position_xy)
 
 	def h(self,node,goal):
-		dx = abs(node.x - goal.x)
-		dy = abs(node.y - goal.y)
+		dx = abs(node.position_xy[0] - goal.position_xy[0])
+		dy = abs(node.position_xy[1] - goal.position_xy[1])
 		return dx+dy
 
+try:
+	astar = Astar(start_point_xy = [-10,-10], goal_points_xy=[[-10,-11],[-2,2],[3,8],[20,80],[1,1]])
+	print "job done"
+	print astar.prioritizedGoals
+except KeyboardInterrupt:
+	pass
 
 w, h = 3, 2
 Matrix = [[0 for x in range(w)] for y in range(h)]
